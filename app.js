bfdad74f43c16ce1c6bdf0fd17559c6b9b99b522
Hamza Afraiz <abbacce@gmail.com>
2021-10-11 05:10:06 -0700

    update js

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ mongoose.connect(process.env.CONNECTION_STRING, {
 })
 
 //Server
-app.listen(3000, ()=>{
+app.listen(process.env.port || 3000, ()=>{
 
     console.log('server is running http://localhost:3000');
 })
