62b665932f56173b8f49b83db8b3f9dd84e23f09
Hamza Afraiz <abbacce@gmail.com>
2021-12-04 07:56:30 -0800

    auth token changed

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 router.get(`/`, async (req, res) =>{
    
-    res.status(200);
+    res.send("wellcome");
 })
 
 module.exports =router;
